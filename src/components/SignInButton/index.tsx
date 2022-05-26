394ae05cc85920132bd787980fee8d9acec90327
Fabiano Alves <fabianoxtx@gmail.com>
2022-05-26 15:00:07 -0300

    SignIn com Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import styles from './styles.module.scss';
-import { FiX } from 'react-icons/fi'; 
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const session = useSession()
     
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+    console.log(session);
+    return session.status === "authenticated" ? (
+        <button
+        className={styles.signInButton}
+        type="button"
+        onClick={() => signOut()}
+        >
             <FaGithub color='#04d361'/>
-            Fabiano Alves
+                {session.data.user.name}
             <FiX className={styles.closeIcon} color='#737380'/>
         </button>
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button 
+        className={styles.signInButton}
+        type="button"
+        onClick={() => signIn('github')}
+        >
             <FaGithub color='#eba417'/>
             Sign in with Github
         </button>

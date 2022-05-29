ea132ef5c06323a62585b697595bd0686b5803fc
Fabiano Alves <fabianoxtx@gmail.com>
2022-05-29 02:42:55 -0300

    Correções na Index e FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -6,7 +6,6 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 export function SignInButton() {
     const session = useSession()
     
-    console.log(session);
     return session.status === "authenticated" ? (
         <button
         className={styles.signInButton}
@@ -24,7 +23,7 @@ export function SignInButton() {
         onClick={() => signIn('github')}
         >
             <FaGithub color='#eba417'/>
-            Sign in with Github
+                Sign in with Github
         </button>
     )
 }

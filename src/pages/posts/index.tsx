1ede44a7b3ff617417c3fec49c4517ae05e97a4b
Fabiano Alves <fabianoxtx@gmail.com>
2022-06-03 03:36:25 -0300

    Página Post(único) e estilização

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -7,6 +7,7 @@ import { RichText } from 'prismic-dom';
 
 
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 type Post = {
 
@@ -30,11 +31,13 @@ export default function PostsList({ posts }: PostsProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => (
-                         <a key={post.slug}>
-                         <time>{post.updatedAt}</time>
-                         <strong>{post.title}</strong>
-                         <p>{post.excerpt}</p>
-                     </a>
+                         <Link key={post.slug} href={`/posts/${post.slug}`}> 
+                            <a>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                     </Link>
                     ))}
                    
                 </div>
